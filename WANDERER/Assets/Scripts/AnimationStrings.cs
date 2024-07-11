c7e301de3fde754cdc4a7d7dd6a7a5d0499f1b3b
czeria <longtlhps38519@gmail.com>
2024-07-12 00:09:32 +0700

    update animation

--- WANDERER/Assets/Scripts/AnimationStrings.cs
+++ WANDERER/Assets/Scripts/AnimationStrings.cs
@@ -6,9 +6,14 @@ using System.Threading.Tasks;
 
 internal class AnimationStrings
 {
-    internal static string run = "run";
-    internal static string grounded = "grounded";
-    internal static string jump = "jump";
+    internal static string jumpTrigger = "jumpTrigger";
     internal static string yVelocity = "yVelocity";
+    internal static string attack1Trigger = "attack1";
+    internal static string canMove = "canMove";
+    internal static string isMoving = "isMoving";
+    internal static string isRunning = "isRunning";
+    internal static string isGrounded = "isGrounded";
+    internal static string isOnWall = "isOnWall";
+    internal static string isOnCeiling = "isOnCeiling";
 }
 
--- WANDERER/Assets/Scripts/PlayerMovement.cs
+++ WANDERER/Assets/Scripts/PlayerMovement.cs
@@ -2,104 +2,170 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.VisualScripting;
+using UnityEngine.InputSystem;
+using System;
 
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float airSpeed;
     [SerializeField] private float jumpPower;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask wallLayer;
-    private Rigidbody2D body;
-    private Animator anim;
+    private Rigidbody2D rb;
+    private Animator animator;
     private CapsuleCollider2D capsuleCollider;
-    private float wallJumpCooldown;
-    private float horizontalInput;
-    
+    Vector2 moveInput;
+    TouchingDirections touchingDirections;
 
-  
+    public float currentMoveSpeed
+    {
+        get
+        {
+            if (IsMoving && !touchingDirections.IsOnWall)
+            {
+                if (touchingDirections.IsGrounded)
+                {
+                    if (IsRunning)
+                    {
+                        return runSpeed;
+                    }
+                    else
+                    {
+                        return walkSpeed;
+                    }
+                }
+                else
+                {
+                    return airSpeed;
+                }
+            }
+            {
+                return 0;
+            }
+        }
+    }
+
+    [SerializeField]
+    private bool _isMoving = false;
+    public bool IsMoving
+    {
+        get
+        {
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool(AnimationStrings.isMoving, value);
+        }
+    }
+
+    [SerializeField]
+    private bool _isRunning = false;
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        private set
+        {
+            _isRunning = value;
+            animator.SetBool(AnimationStrings.isRunning, value);
+        }
+    }
+
+
+    public bool canMove { get
+        {
+            return animator.GetBool(AnimationStrings.canMove);
+        } }
+
+    public bool _isFacingRight = true;
+    public bool IsFacingRight
+    {
+        get
+        {
+            return _isFacingRight;
+        }
+        private set
+        {
+            if(IsFacingRight!=value)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+            }
+
+            _isFacingRight = value;
+        }
+    }
+
     private void Awake()
     {
         //grab references for rigidbody and animator from object
-        body = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         capsuleCollider = GetComponent<CapsuleCollider2D>();
+        touchingDirections = GetComponent<TouchingDirections>();
     }
 
 
-    private void Update()
+    private void FixedUpdate()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        
+        rb.velocity = new Vector2(moveInput.x * currentMoveSpeed, rb.velocity.y);
 
-        //flip player when moving left-right
-        if(horizontalInput > 0.01f)
-            transform.localScale = Vector3.one;
-        else if(horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-1, 1, 1);
-
-        //set animator parameters
-        anim.SetBool(AnimationStrings.run, horizontalInput != 0);
-        anim.SetBool(AnimationStrings.grounded, isGrounded());
-        anim.SetFloat(AnimationStrings.yVelocity, body.velocity.y);
-
-        //wall jump logic
-        if (wallJumpCooldown > 0.2f)
-        {
-            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-
-            if (onWall() && !isGrounded())
-            {
-                body.gravityScale = 0;
-                body.velocity = Vector2.zero;
-            }
-            else
-                body.gravityScale = 5;
-
-            if (Input.GetKey(KeyCode.Space))
-                Jump();
-        }
-        else
-            wallJumpCooldown += Time.deltaTime;
+        animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
     }
 
-    private void Jump()
+    public void onAttack(InputAction.CallbackContext context)
     {
-        if (isGrounded())
+        if(context.started)
         {
-            body.velocity = new Vector2(body.velocity.x, jumpPower);
-            anim.SetTrigger(AnimationStrings.jump);
-        }
-       else if (onWall() && !isGrounded())
-        {
-            if(horizontalInput == 0)
-            {
-                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
-                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x),transform.localScale.y, transform.localScale.z);
-            }
-            else
-                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
-
-            wallJumpCooldown = 0;
-            
+            animator.SetTrigger(AnimationStrings.attack1Trigger);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    public void onRun(InputAction.CallbackContext context)
     {
+        if(context.started)
+        {
+            IsRunning = true;
+        }else if(context.canceled)
+        {
+            IsRunning = false;
+        }
     }
 
-    private bool isGrounded()
+    public void onMove(InputAction.CallbackContext context)
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(capsuleCollider.bounds.center, capsuleCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-        return raycastHit.collider != null;
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
     }
 
-    private bool onWall()
+    private void SetFacingDirection(Vector2 moveInput)
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(capsuleCollider.bounds.center, capsuleCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
-        return raycastHit.collider != null;
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
     }
 
-    
+    public void onJump(InputAction.CallbackContext context)
+    {
+        if(context.started && touchingDirections.IsGrounded)
+        {
+            animator.SetTrigger(AnimationStrings.jumpTrigger);
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+    }
 }
