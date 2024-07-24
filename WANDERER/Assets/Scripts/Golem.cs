2fbb15871d5b84a9dca7dc99c8ef06124d2a2523
czeria <longtlhps38519@gmail.com>
2024-07-24 16:05:38 +0700

    update attackk combo

--- WANDERER/Assets/Scripts/Golem.cs
+++ WANDERER/Assets/Scripts/Golem.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class Golem : MonoBehaviour
 {
-    public float walkSpeed = 3f;
+    public float walkAcceleration = 3f;
+    public float maxSpeed = 3f;
     public float walkStopRate = 0.05f;
     public DetectionZone attackZone;
     public DetectionZone cliffDetectionZone;
@@ -99,7 +100,7 @@ public class Golem : MonoBehaviour
         if (!damageAble.lockVelocity)
         {
             if (CanMove)
-                rb.velocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.velocity.y);
+                rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x + (walkAcceleration * walkDirectionVector.x * Time.fixedDeltaTime), -maxSpeed, maxSpeed), rb.velocity.y);
             else
                 rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, walkStopRate), rb.velocity.y);
         }
