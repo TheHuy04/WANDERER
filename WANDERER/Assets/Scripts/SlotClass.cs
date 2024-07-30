7ab264d975ece49ee3ae678af5c22fc89468884d
TheHuy04 <huyparma123@gmail.com>
2024-07-30 12:14:24 +0700

    tui do

---
+++ WANDERER/Assets/Scripts/SlotClass.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+[System.Serializable]
+
+public class SlotClass
+{
+    [SerializeField]private ItemClass item;
+    [SerializeField]private int quatity = 0;
+
+    public SlotClass()
+    {
+        item = null;
+        quatity = 0;
+    }
+    public SlotClass(ItemClass _item, int _quality)
+    {
+        this.item = _item;
+        this.quatity = _quality;
+    }
+    public ItemClass GetItem() { return this.item; }
+    public int GetQuatity() { return this.quatity; }
+    public void AddQuatity( int _quatity) { quatity = quatity + _quatity; }
+    public void SubQuatity( int _quatity) { quatity = quatity - _quatity; }
+    public void AddItem(ItemClass item, int quantity)
+    {
+        this.item = item;
+        this.quatity = quantity;
+    }
+
+    public void RemoveItem()
+    {
+        this.item = null;
+        this.quatity = 0;
+    }
+} 
