e42fda45928ed1005caf29d4d9fde6a67fb17729
czeria <longtlhps38519@gmail.com>
2024-07-30 11:32:42 +0700

    add teleport gate

---
+++ WANDERER/Assets/Sprite/Pixel Art Asset - Portal Monolitic 3.0/readme.txt.txt
@@ -0,0 +1,11 @@
+Thank you for downloading and enjoy exploring my pixel art!
+
+I am a passionate pixel artist who loves exploring and sharing the magic of pixel art. Here, I present some of my pixel creations, 
+inspired by nostalgic video games and modern interpretations of classic pixel styles. 
+
+My focus is on offering you free artworks to enjoy and use. It brings me joy to see my work appreciated and utilized by others. 
+
+I hope you like my art and find something that inspires you. Feel free to comment on and rate my work. 
+Your feedback is important to me and helps me to develop and create better artworks.
+
+All my Art ist Handmade. I dont use AI, to create my Art!
---
+++ WANDERER/Assets/TriggerAnimationAndTeleport.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerAnimationAndTeleport : MonoBehaviour
+{
+    public string animationTriggerName = "Teleport"; // Name of the trigger parameter in your Animator
+    public string nextSceneName = "_MainMenu"; // Name of the scene to load
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("No Animator component found on this GameObject.");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // Assuming the player has the tag "Player"
+        {
+            StartCoroutine(TriggerAnimationAndTeleportCoroutine());
+        }
+    }
+
+    IEnumerator TriggerAnimationAndTeleportCoroutine()
+    {
+        animator.SetTrigger(animationTriggerName); // Trigger the animation
+        yield return new WaitForSeconds(4f); // Wait for 2 seconds
+        SceneManager.LoadScene(nextSceneName); // Load the next scene
+    }
+}
